2c19985b4206b0485b4054d94b5d5f02181c0d33
Duman <dumantoleu01@gmail.com>
2021-04-07 13:42:44 +0600

    new screen

--- lib/src/screens/auth/sign_in_validation.dart
+++ lib/src/screens/auth/sign_in_validation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foody/src/common/widgets/appbar/appbar.dart';
 
 class SignInValidation extends StatefulWidget {
   SignInValidation({Key key}) : super(key: key);
@@ -10,8 +11,8 @@ class SignInValidation extends StatefulWidget {
 class _SignInValidationState extends State<SignInValidation> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Duman'),
+    return Scaffold(
+      appBar: AppBarTitle(title: 'Подтверждение'),
     );
   }
 }
